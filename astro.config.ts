d240d9a23b869d07cae70a959507de89cf7bfb01
Ben Silverman <bensilverman10@gmail.com>
2026-05-06 10:16:17 +0100

    move font to astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
@@ -13,4 +13,43 @@ export default defineConfig({
 	site: "https://bensilverman.co.uk",
 	adapter: netlify(),
 	prefetch: true,
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Atkinson Hyperlegible Next",
+			cssVariable: "--atkinson-hyperlegible-next",
+			options: {
+				variants: [
+					{
+						src: [
+							"./src/assets/fonts/AtkinsonHyperlegibleNext-VariableFont_wght.ttf",
+						],
+						weight: "100 1000",
+						style: "normal",
+					},
+					{
+						src: [
+							"./src/assets/fonts/AtkinsonHyperlegibleNext-Italic-VariableFont_wght.ttf",
+						],
+						weight: "100 1000",
+						style: "italic",
+					},
+				],
+			},
+		},
+		{
+			provider: fontProviders.local(),
+			name: "JetBrains Mono Regular",
+			cssVariable: "--jetbrains-mono",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/JetBrainsMono-Regular.woff2"],
+						weight: "normal",
+						style: "normal",
+					},
+				],
+			},
+		},
+	],
 });
